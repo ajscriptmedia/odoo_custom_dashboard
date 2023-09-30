20027fc6eefafac760ee6734a88483be87969dc5
ajscriptmedia <ajscriptmedia@gmail.com>
2023-10-01 01:45:09 +0400

    [ADD] retrieve data usign orm and add action service

--- static/src/components/sales_dashboard.js
+++ static/src/components/sales_dashboard.js
@@ -4,11 +4,152 @@ import { registry } from "@web/core/registry"
 import { KpiCard } from "./kpi_card/kpi_card"
 import { ChartRenderer } from "./chart_renderer/chart_renderer"
 import { loadJS } from "@web/core/assets"
-const { Component, onWillStart, useRef, onMounted } = owl
+import { useService } from "@web/core/utils/hooks"
+const { Component, onWillStart, useRef, onMounted, useState } = owl
 
 export class OwlSalesDashboard extends Component {
     setup(){
+        this.state = useState({
+            quotations: {
+                value:10,
+                percentage:6,
+            },
+            period:90,
+        })
+        this.orm = useService("orm")
+        this.actionService = useService("action")
 
+        onWillStart(async ()=>{
+            this.getDates()
+            await this.getQuotations()
+            await this.getOrders()
+        })
+    }
+
+    async onChangePeriod(){
+        this.getDates()
+        await this.getQuotations()
+        await this.getOrders()
+    }
+
+    getDates(){
+        this.state.current_date = moment().subtract(this.state.period, 'days').format('L')
+        this.state.previous_date = moment().subtract(this.state.period * 2, 'days').format('L')
+    }
+
+    async getQuotations(){
+        let domain = [['state', 'in', ['sent', 'draft']]]
+        if (this.state.period > 0){
+            domain.push(['date_order','>', this.state.current_date])
+        }
+        const data = await this.orm.searchCount("sale.order", domain)
+        this.state.quotations.value = data
+
+        // previous period
+        let prev_domain = [['state', 'in', ['sent', 'draft']]]
+        if (this.state.period > 0){
+            prev_domain.push(['date_order','>', this.state.previous_date], ['date_order','<=', this.state.current_date])
+        }
+        const prev_data = await this.orm.searchCount("sale.order", prev_domain)
+        const percentage = ((data - prev_data)/prev_data) * 100
+        this.state.quotations.percentage = percentage.toFixed(2)
+    }
+
+    async getOrders(){
+        let domain = [['state', 'in', ['sale', 'done']]]
+        if (this.state.period > 0){
+            domain.push(['date_order','>', this.state.current_date])
+        }
+        const data = await this.orm.searchCount("sale.order", domain)
+        //this.state.quotations.value = data
+
+        // previous period
+        let prev_domain = [['state', 'in', ['sale', 'done']]]
+        if (this.state.period > 0){
+            prev_domain.push(['date_order','>', this.state.previous_date], ['date_order','<=', this.state.current_date])
+        }
+        const prev_data = await this.orm.searchCount("sale.order", prev_domain)
+        const percentage = ((data - prev_data)/prev_data) * 100
+        //this.state.quotations.percentage = percentage.toFixed(2)
+
+        //revenues
+        const current_revenue = await this.orm.readGroup("sale.order", domain, ["amount_total:sum"], [])
+        const prev_revenue = await this.orm.readGroup("sale.order", prev_domain, ["amount_total:sum"], [])
+        const revenue_percentage = ((current_revenue[0].amount_total - prev_revenue[0].amount_total) / prev_revenue[0].amount_total) * 100
+
+        //average
+        const current_average = await this.orm.readGroup("sale.order", domain, ["amount_total:avg"], [])
+        const prev_average = await this.orm.readGroup("sale.order", prev_domain, ["amount_total:avg"], [])
+        const average_percentage = ((current_average[0].amount_total - prev_average[0].amount_total) / prev_average[0].amount_total) * 100
+
+        this.state.orders = {
+            value: data,
+            percentage: percentage.toFixed(2),
+            revenue: `$${(current_revenue[0].amount_total/1000).toFixed(2)}K`,
+            revenue_percentage: revenue_percentage.toFixed(2),
+            average: `$${(current_average[0].amount_total/1000).toFixed(2)}K`,
+            average_percentage: average_percentage.toFixed(2),
+        }
+
+        //this.env.services.company
+    }
+
+    async viewQuotations(){
+        let domain = [['state', 'in', ['sent', 'draft']]]
+        if (this.state.period > 0){
+            domain.push(['date_order','>', this.state.current_date])
+        }
+
+        let list_view = await this.orm.searchRead("ir.model.data", [['name', '=', 'view_quotation_tree_with_onboarding']], ['res_id'])
+
+        this.actionService.doAction({
+            type: "ir.actions.act_window",
+            name: "Quotations",
+            res_model: "sale.order",
+            domain,
+            views: [
+                [list_view.length > 0 ? list_view[0].res_id : false, "list"],
+                [false, "form"],
+            ]
+        })
+    }
+
+    viewOrders(){
+        let domain = [['state', 'in', ['sale', 'done']]]
+        if (this.state.period > 0){
+            domain.push(['date_order','>', this.state.current_date])
+        }
+
+        this.actionService.doAction({
+            type: "ir.actions.act_window",
+            name: "Quotations",
+            res_model: "sale.order",
+            domain,
+            context: {group_by: ['date_order']},
+            views: [
+                [false, "list"],
+                [false, "form"],
+            ]
+        })
+    }
+
+    viewRevenues(){
+        let domain = [['state', 'in', ['sale', 'done']]]
+        if (this.state.period > 0){
+            domain.push(['date_order','>', this.state.current_date])
+        }
+
+        this.actionService.doAction({
+            type: "ir.actions.act_window",
+            name: "Quotations",
+            res_model: "sale.order",
+            domain,
+            context: {group_by: ['date_order']},
+            views: [
+                [false, "pivot"],
+                [false, "form"],
+            ]
+        })
     }
 }
 
